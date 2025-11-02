4789806138bddfdf0ab6cc0dd602f902c0a1fdf3
Byul Kang <halococoa@gmail.com>
2025-11-03 02:28:29 +0900

    init

---
+++ BaseVerifier/BaseVerifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseVerifierApp.swift
+//  BaseVerifier
+//
+//  Created by Byul Kang on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct BaseVerifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseVerifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaseVerifier
+//
+//  Created by Byul Kang on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
